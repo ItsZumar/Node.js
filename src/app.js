2351d789ad2b7b6b9faa63d2475701b850150acc
ItsZumar <zumarsaeed17@gmail.com>
2022-01-28 19:00:01 +0500

    changes

--- src/app.js
+++ src/app.js
@@ -60,7 +60,7 @@ app.get('/products', (req, res) => {
 app.get('/weather', (req, res) => {
     if(!req.query.address) {
         return res.send({
-            error: "Error msg."
+            error: "Error msg!"
         })
     }
 
